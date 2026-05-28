68809ec51c931d62f67d923587bc0b6360c85c19
Sabhya Kumar <sabhyakumar@Sabhyas-Laptop.local>
2026-05-28 16:57:43 +0530

    fix: allow all origins

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://ps1-project-ai-debugging-assistant.vercel.app"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
